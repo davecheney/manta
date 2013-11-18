788ca2bcf1c4b6bdf3564539c11135fb17d3c109
Dave Cheney <dave@cheney.net>
2013-11-18 16:41:36 +1100

    more tests

--- manta.go
+++ manta.go
@@ -68,10 +68,10 @@ func (c *Client) SignRequest(req *http.Request) error {
 			return fmt.Errorf("could not load private key %q: %v", c.Key, err)
 		}
 	}
-	return signRequest(req, c.User, c.KeyId, c.signer)
+	return signRequest(req, fmt.Sprintf("/%s/keys/%s", MANTA_USER, MANTA_KEY_ID), c.signer)
 }
 
-func signRequest(req *http.Request, MANTA_USER, MANTA_KEY_ID string, priv Signer) error {
+func signRequest(req *http.Request, keyid string, priv Signer) error {
 	now := time.Now().UTC().Format(time.RFC1123)
 	req.Header.Set("date", now)
 	signed, err := priv.Sign([]byte(fmt.Sprintf("date: %s", now)))
@@ -79,7 +79,7 @@ func signRequest(req *http.Request, MANTA_USER, MANTA_KEY_ID string, priv Signer
 		return fmt.Errorf("could not sign request: %v", err)
 	}
 	sig := base64.StdEncoding.EncodeToString(signed)
-	authz := fmt.Sprintf("Signature keyId=%q,algorithm=%q,signature=%q", fmt.Sprintf("/%s/keys/%s", MANTA_USER, MANTA_KEY_ID), "rsa-sha256", sig)
+	authz := fmt.Sprintf("Signature keyId=%q,algorithm=%q,signature=%q", keyid, "rsa-sha256", sig)
 	req.Header.Set("Authorization", authz)
 	return nil
 }
--- manta_test.go
+++ manta_test.go
@@ -2,7 +2,9 @@ package manta
 
 import (
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
@@ -35,5 +37,61 @@ func TestPrivateKeySign(t *testing.T) {
 	const want = "ATp0r26dbMIxOopqw0OfABDT7CKMIoENumuruOtarj8n/97Q3htHFYpH8yOSQk3Z5zh8UxUym6FYTb5+A0Nz3NRsXJibnYi7brE/4tx5But9kkFGzG+xpUmimN4c3TMN7OFH//+r8hBf7BT9/GmHDUVZT2JzWGLZES2xDOUuMtA="
 	if got := base64.StdEncoding.EncodeToString(sig); got != want {
 		t.Fatalf("want: %q, got %q", want, got)
+	}
+}
+
+func TestSignRequest(t *testing.T) {
+	priv, err := loadPrivateKey("_testdata/id_rsa")
+	if err != nil {
+		t.Fatal(err)
+	}
+	req, err := http.NewRequest("GET", "http://example.com/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	signRequest(req, "Test", priv)
+	date := req.Header.Get("date")
+	sig, err := priv.Sign([]byte("date: " + date))
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := fmt.Sprintf("Signature keyId=%q,algorithm=%q,signature=%q", "Test", "rsa-sha256", base64.StdEncoding.EncodeToString(sig))
+	if got := req.Header.Get("Authorization"); got != want {
+		t.Fatalf("want: %q, got: %q", want, got)
+	}
+}
+
+func TestClientNewRequest(t *testing.T) {
+	client := Client{
+		User:  "test",
+		KeyId: "q",
+		Key:   "_testdata/id_rsa",
+		Url:   "http://example.com",
+	}
+	req, err := client.NewRequest("GET", "/test/public", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := req.URL.Host, "example.com"; got != want {
+		t.Errorf("want: %q, got: %q", want, got)
+	}
+	if got, want := req.URL.Path, "/test/public"; got != want {
+		t.Errorf("want: %q, got: %q", want, got)
+	}
+}
+
+func TestClientSignRequest(t *testing.T) {
+	client := Client{
+		User:  "test",
+		KeyId: "q",
+		Key:   "_testdata/id_rsa",
+		Url:   "http://example.com",
+	}
+	req, err := client.NewRequest("GET", "/test/public", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := client.SignRequest(req); err != nil {
+		t.Fatal(err)
 	}
 }
