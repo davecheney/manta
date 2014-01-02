799a2f78ce5b47f2374a43ad5272c840acd78644
Reed O'Brien <reed@reedobrien.com>
2014-01-02 00:08:18 -0500

    Add a test to cover shared flag support
    This doesnt' test much except that there are three expected flags. If
    that value changes it will fail.
    
    I am not sure what else should get tested. If someone has their env set
    the values would be differnet than either if it is not or if someone else
    runs the test with their env set.
    
    I suppose the test could unset those env variables and run, testing the
    default values get set, and then reset them?

---
+++ flag_test.go
@@ -0,0 +1,25 @@
+// Copyright 2014 manta AUTHORS.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package manta
+
+import (
+	"flag"
+	"testing"
+)
+
+var SHARED_FLAGS int = 3
+var count int
+
+func printFlag(f *flag.Flag) {
+	count += 1
+}
+
+func TestDefaultFlags(t *testing.T) {
+	flags := Flags()
+	flags.VisitAll(printFlag)
+	if count != SHARED_FLAGS {
+		t.Errorf("Expected default flag count %i, found %i", SHARED_FLAGS, count)
+	}
+}
